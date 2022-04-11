312783fa61b873c0b34768b6db9540fffe25b6ee
sper0054 <teresa.sperl@gmail.com>
2022-04-11 10:59:11 -0500

    setting up layout and files

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg';
+import coverImage from '../../assets/tiles.jpeg'
+
 function About() {
   return (
     <section className="my-5">
-      <h1 id="about">Who am I?</h1>
+      <h1 id="about">About Me</h1>
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       <div className="my-2">
         <p>
-          This is the paragraph about me.
+        By day, I am a lead product owner at Target. By night, I am a student learning full-stack web development. I have been with Target for more than 10 years in various roles. In my spare time (when I am not learning how to code) I enjoy playing co-rec sports, baking, and spending time with my family.
         </p>
       </div>
     </section>
