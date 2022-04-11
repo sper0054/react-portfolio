07b4d21a823e37ba7cbc3e23f3eb1be068f6bdce
sper0054 <teresa.sperl@gmail.com>
2022-04-11 10:40:50 -0500

    about and pictures added

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import coverImage from '../../assets/cover/cover-image.jpg';
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          This is the paragraph about me.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
