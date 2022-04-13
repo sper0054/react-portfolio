20a47d4caa169253128ee6cfb6a4aa0b18ffaebe
sper0054 <teresa.sperl@gmail.com>
2022-04-12 20:13:25 -0500

    footer adjustment'

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,12 +5,14 @@ function Footer() {
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-          <strong>My Full Stack Development Portfolio</strong> - a React site by{" "}
-          <a href="https://github.com/sper0054" target="_blank" rel="noreferrer">
-            Teresa Sperl Wohl
-          </a>
-          .
           <hr />
+          <a
+            href="https://github.com/sper0054"
+            target="_blank" rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          |{" "}{" "}
           <a
             href="https://www.linkedin.com/in/teresawohl/"
             target="_blank" rel="noreferrer"
