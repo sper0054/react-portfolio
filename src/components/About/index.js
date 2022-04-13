a62d86aa093331be3df411ac162ed6f00aa4e22c
sper0054 <teresa.sperl@gmail.com>
2022-04-12 20:23:39 -0500

    css start

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <div>
       <h1>Dabbling in Web Development</h1>
-      <img src={myPicture} className="my-2" style={{ width: "10%" }} alt="Teresa" />
+      <img src={myPicture} className="my-pic" style={{ width: "10%" }} alt="Teresa" />
       <p className="content">
       By day, I am a lead product owner at Target. By night, I am a student learning full-stack web development. I have been with Target for 12 years in various roles. In my spare time (when I am not learning how to code) I enjoy playing co-rec sports, baking, and spending time with my family.
       </p>
